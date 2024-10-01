b791861616df51e1cffe406ca2cb5b1186fd3808
Alejandro Rojas <cidorcorojas@gmail.com>
2024-10-01 11:32:41 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Button from './components/button'
+
+function App() {
+
+  return (
+    <>
+      <main className="">
+      <Button value={1}></Button>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({value}) => {
+  return (
+    <>
+    <button className='border-2 border-black size-10 font-medium m-2'>{value}</button>
+    </>
+  )
+}
+
+export default Button
